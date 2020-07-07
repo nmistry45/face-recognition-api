a00c6a1fcc58ac5eacfa9d554d9f574a2281001c
nmistry45 <nmistry45@gmail.com>
2020-07-07 17:15:43 +0530

    deploying to heroku and github pages

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
-	console.log("App is Running on Port 3001");
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`App is Running on Port ${process.env.PORT}`);
 })
