dcfffacb8d2d3f420863bfd7d86388f43b81dcfa
nmistry45 <nmistry45@gmail.com>
2020-07-07 17:17:42 +0530

    deploying to heroku and github pages

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('It is Working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
